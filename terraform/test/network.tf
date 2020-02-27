d6c07b90611e0c67b1027e9cce84a2e0c72c0387
Tine Kleivane <tine.kleivane@bekk.no>
2020-02-27 12:57:19 +0100

    nettverking til å laste ned image fra ecr for ecs

--- terraform/test/network.tf
+++ terraform/test/network.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "private" {
 
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "public" {
   count             = var.az_count
   cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, var.az_count + count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
@@ -28,11 +28,47 @@ resource "aws_subnet" "private" {
 
 }
 
+# Internet Gateway for the public subnet
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = module.bekk_test_static_json_label.tags
+}
+
+resource "aws_route_table" "public_internet" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = module.bekk_test_static_json_label.tags
+
+}
+
+# Explicitly associate the newly created route tabless to the public subnets (so they don't default to the main route table)
+resource "aws_route_table_association" "public" {
+  count          = var.az_count
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public_internet.id
+}
+
+# Create a NAT gateway with an Elastic IP for each private subnet to get internet connectivity
+resource "aws_eip" "nat_ip" {
+  count      = var.az_count
+  vpc        = true
+  depends_on = [aws_internet_gateway.gw]
+  tags       = module.bekk_test_static_json_label.tags
+}
 
 resource "aws_nat_gateway" "gw" {
   count         = var.az_count
   subnet_id     = element(aws_subnet.public.*.id, count.index)
-  allocation_id = element(aws_eip.gw.*.id, count.index)
+  allocation_id = element(aws_eip.nat_ip.*.id, count.index)
+
+  tags = module.bekk_test_static_json_label.tags
+
 }
 
 # Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
@@ -44,6 +80,8 @@ resource "aws_route_table" "private" {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = element(aws_nat_gateway.gw.*.id, count.index)
   }
+  tags = module.bekk_test_static_json_label.tags
+
 }
 
 # Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
